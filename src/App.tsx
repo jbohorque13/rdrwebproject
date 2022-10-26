a53556e142aaf617d9d1fab90b52114d98cd10ef
Josue Bohorquez <jbohorque13@users.noreply.github.com>
2022-10-26 18:33:27 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
 import React from 'react';
-import { Link, Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar/Sidebar';
+import { routes } from './utils/routesHelper';
+
+const InnerApp: React.FC = React.memo(() => {
+  const renderRoutes = (
+    <React.Suspense>
+      <Routes>
+        {Object.values(routes).map(route => (
+            <Route path={route.path} />
+        ))}
+      </Routes>
+    </React.Suspense>
+  )
+
+  return (
+    <>
+      <div>
+        {renderRoutes}
+      </div>
+    </>
+  )
+})
 
 const App: React.FC = () => {
+
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/dashboard">
-              Dashboard
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path='/' element={<div> Home </div>} />
-        <Route path='/dashboard' element={<div>Dashboard</div>} />
-      </Routes>
+      <Sidebar />
+      <InnerApp />
     </div>
   )
 }
