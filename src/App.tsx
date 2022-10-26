fc171a15708fee942501a354da0938d131c05823
Josue Bohorquez <jbohorque13@users.noreply.github.com>
2022-10-26 17:17:48 -0300

    started

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link, Routes, Route } from 'react-router-dom';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/dashboard">
+              Dashboard
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<div> Home </div>} />
+        <Route path='/dashboard' element={<div>Dashboard</div>} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
