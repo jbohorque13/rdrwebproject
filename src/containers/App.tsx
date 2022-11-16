cd27422f18defe7dd3f9ab927afc8972b6f68828
Josue Bohorquez <jbohorque13@users.noreply.github.com>
2022-11-16 14:50:45 -0300

    wip

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+// hooks
+import { useLocation } from 'react-router-dom';
 import Sidebar from '../components/Sidebar/Sidebar';
 import ProviderAuth from '../contexts/auth';
 import { routes } from '../utils/routesHelper';
@@ -13,6 +15,19 @@ import {
 } from './style'
 
 const InnerApp: React.FC = React.memo(() => {
+  // Hooks
+  const location = useLocation();
+  // Effects
+  React.useEffect(() => {
+    // Save token redirectoToDate and redirect on local storage.
+    if(location.hash !== null && location.pathname.startsWith('/index.html') && !localStorage.getItem('rdr-token')) {
+      localStorage.setItem('rdr-redirectToDate', Date.now().toString());
+      localStorage.setItem('rdr-redirectTo', location.pathname);
+      localStorage.setItem('rdr-token', location.hash.slice(location.hash.indexOf('=') + 1, location.hash.length));
+    }
+
+  }, [location]);
+  
   const renderRoutes = (
     <React.Suspense>
       <Routes>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,6 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-const Home = () => {
-  const { id_token } = useParams();
-  console.log('id_token ', id_token);
+import React from 'react';
 
+const Home = () => {
   return (
     <div>Inicio</div>
   )
